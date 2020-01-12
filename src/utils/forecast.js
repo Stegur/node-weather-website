96b4b124d67539bbc51b1da5e285e944c6d5d6c6
Stegura Alex <stegura.alex@gmail.com>
2020-01-13 00:01:41 +0300

    Change language

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `https://api.darksky.net/forecast/13d987d04d36eac4aa68960e535d9e8d/${latitude},${longitude}?units=si`;
+    const url = `https://api.darksky.net/forecast/13d987d04d36eac4aa68960e535d9e8d/${latitude},${longitude}?units=si&lang=ru`;
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Локация не найдена', undefined);
         } else {
-            callback(undefined, `${body.daily.data[0].summary} Now ${body.currently.temperature}°C. Rain posibility ${body.currently.precipProbability}%`);
+            callback(undefined, `${body.daily.data[0].summary} Сейчас ${body.currently.temperature}°C. Вероятность дождя ${body.currently.precipProbability}%`);
         }
     });
 };
